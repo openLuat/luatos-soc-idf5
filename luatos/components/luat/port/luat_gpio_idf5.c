6ba1a886ec7a1abb4e175bd1619a71219c918ad7
Wendal Chen <wendal1985@gmail.com>
2022-10-03 23:32:12 +0800

    update: gpio补齐注释,另补齐gpio输出初始状态值

--- luatos/components/luat/port/luat_gpio_idf5.c
+++ luatos/components/luat/port/luat_gpio_idf5.c
@@ -7,104 +7,143 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
-#define LUAT_LOG_TAG "luat.gpio"
+#define LUAT_LOG_TAG "gpio"
 #include "luat_log.h"
 
 #if CONFIG_IDF_TARGET_ESP32C3
 #define GPIO_MAX (21)
 #else
-#define GPIO_MAX (-1)
+#define GPIO_MAX (32)
 #endif
 
-#define GPIO_CHECK(pin) ((pin<0||pin>GPIO_MAX)?-1:0)
+#define GPIO_CHECK(pin) ((pin<0 || pin>GPIO_MAX) ? -1:0)
 
-static void IRAM_ATTR gpio_isr_handler(void *arg){
+static uint8_t warn_gpio18 = 0;
+static uint8_t warn_gpio19 = 0;
+static uint8_t gpio_isr_sta = 0;
+
+static void IRAM_ATTR gpio_isr_handler(void *arg) {
     uint32_t pin = (uint32_t)arg;
     luat_gpio_irq_default(pin, (void*)luat_gpio_get(pin));
 }
 
-int luat_gpio_setup(luat_gpio_t *gpio){
-    static uint8_t gpio_isr_sta = 0;
-    if (GPIO_CHECK(gpio->pin)){
+int luat_gpio_setup(luat_gpio_t *gpio) {
+    if (GPIO_CHECK(gpio->pin)) {
+        LLOGW("not such GPIO number %d", gpio->pin);
         return -1;
     }
-    gpio_reset_pin(gpio->pin);
-    if (gpio->mode == Luat_GPIO_OUTPUT){
-        gpio_set_direction(gpio->pin, GPIO_MODE_OUTPUT);
+    int pin = gpio->pin;
+
+    // 如果是GPIO18/19, 这两个2脚也用在USB, 在简约版会导致USB连接中断
+    if (pin == 18 && warn_gpio18 == 0) {
+        warn_gpio18 = 1;
+        LLOGI("GPIO 18/19 is use for USB too");
     }
-    else if (gpio->mode == Luat_GPIO_INPUT){
-        gpio_set_direction(gpio->pin, GPIO_MODE_INPUT);
+    if (pin == 19 && warn_gpio19 == 0) {
+        warn_gpio19 = 1;
+        LLOGI("GPIO 18/19 is use for USB too");
     }
-    else if (gpio->mode == Luat_GPIO_IRQ){
-        if (gpio_isr_sta == 0){
+    // 重置管脚的功能为GPIO
+    gpio_reset_pin(pin);
+
+    // 设置输出/输入/中断状态
+    if (gpio->mode == Luat_GPIO_OUTPUT) {
+        // 输出模式
+        gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+        // 还得设置初始状态
+        gpio_set_level(pin, gpio->irq); // 输出模式时, gpio-irq兼职输出状态值
+    }
+    else if (gpio->mode == Luat_GPIO_INPUT) {
+        gpio_set_direction(pin, GPIO_MODE_INPUT);
+    }
+    else if (gpio->mode == Luat_GPIO_IRQ) {
+        if (gpio_isr_sta == 0) {
             gpio_install_isr_service(0);
             gpio_isr_sta = 1;
         }
-        gpio_set_direction(gpio->pin, GPIO_MODE_INPUT);
-        switch (gpio->irq){
+        // 中断当然是输入模式了
+        gpio_set_direction(pin, GPIO_MODE_INPUT);
+        switch (gpio->irq) {
+        // 上升沿
         case Luat_GPIO_RISING:
-            gpio_set_intr_type(gpio->pin, GPIO_INTR_POSEDGE);
+            gpio_set_intr_type(pin, GPIO_INTR_POSEDGE);
             break;
+        // 下降沿
         case Luat_GPIO_FALLING:
-            gpio_set_intr_type(gpio->pin, GPIO_INTR_NEGEDGE);
+            gpio_set_intr_type(pin, GPIO_INTR_NEGEDGE);
             break;
+        // 高电平
+        case Luat_GPIO_HIGH_IRQ:
+            gpio_set_intr_type(pin, GPIO_INTR_HIGH_LEVEL);
+            break;
+        // 低电平
+        case Luat_GPIO_LOW_IRQ:
+            gpio_set_intr_type(pin, GPIO_INTR_LOW_LEVEL);
+            break;
+        // 双向触发, 上升沿和下降沿都触发
         case Luat_GPIO_BOTH:
         default:
-            gpio_set_intr_type(gpio->pin, GPIO_INTR_ANYEDGE);
+            gpio_set_intr_type(pin, GPIO_INTR_ANYEDGE);
             break;
         }
-        gpio_isr_handler_add(gpio->pin, gpio_isr_handler, (void *)gpio->pin);
+        gpio_isr_handler_add(pin, gpio_isr_handler, (void *)pin);
     }
-    switch (gpio->pull){
-    case Luat_GPIO_DEFAULT:
-        gpio_set_pull_mode(gpio->pin, GPIO_FLOATING);
-        break;
+    // 上拉/下拉状态
+    switch (gpio->pull) {
     case Luat_GPIO_PULLUP:
-        gpio_set_pull_mode(gpio->pin, GPIO_PULLUP_ONLY);
+        gpio_set_pull_mode(pin, GPIO_PULLUP_ONLY);
         break;
     case Luat_GPIO_PULLDOWN:
-        gpio_set_pull_mode(gpio->pin, GPIO_PULLDOWN_ONLY);
+        gpio_set_pull_mode(pin, GPIO_PULLDOWN_ONLY);
         break;
+    // 默认浮空状态
+    case Luat_GPIO_DEFAULT:
     default:
+        gpio_set_pull_mode(pin, GPIO_FLOATING);
         break;
     }
     return 0;
 }
 
-int luat_gpio_set(int pin, int level){
-    if (GPIO_CHECK(pin)){
+int luat_gpio_set(int pin, int level) {
+    if (pin == Luat_GPIO_MAX_ID)
+        return 0;
+    if (GPIO_CHECK(pin)) {
         return -1;
     }
-    return gpio_set_level(pin, level);
+    gpio_set_level(pin, level);
+    return 0; // 总是返回正常, pin正确不可能失败吧
 }
 
-int luat_gpio_get(int pin){
-    if (GPIO_CHECK(pin)){
-        return -1;
+int luat_gpio_get(int pin) {
+    if (pin == Luat_GPIO_MAX_ID)
+        return 0;
+    if (GPIO_CHECK(pin)) {
+        return 0; // 不存在? 总是返回0, 低电平
     }
     return gpio_get_level(pin);
 }
 
-void luat_gpio_close(int pin){
-    if (GPIO_CHECK(pin)){
+void luat_gpio_close(int pin) {
+    if (GPIO_CHECK(pin)) {
         return;
     }
     gpio_reset_pin(pin);
 }
 
 #include "soc/gpio_reg.h"
-#define GPIO_OUT_DATA	(*(volatile unsigned int*)(GPIO_OUT_REG))
+#define GPIO_OUT_PULSE	(*(volatile unsigned int*)(GPIO_OUT_REG))
 
 void IRAM_ATTR luat_gpio_pulse(int pin, uint8_t *level, uint16_t len, uint16_t delay_ns) {
-    volatile uint32_t del=delay_ns;
+    volatile uint32_t tmp = delay_ns > 1024 ? 1024 : delay_ns;
     vPortEnterCritical();
-    for(int i=0; i<len; i++){
+    for(int i=0; i<len; i++) {
         if(level[i/8]&(0x80>>(i%8)))
-            GPIO_OUT_DATA |= (0x00000001<<pin);
+            GPIO_OUT_PULSE |= (0x00000001<<pin); // TODO 预先计算会不会更好一些, 也更快
         else 
-            GPIO_OUT_DATA &= ~(0x00000001<<pin);
-        del = delay_ns;
-        while(del--);
+            GPIO_OUT_PULSE &= ~(0x00000001<<pin);
+        tmp = delay_ns > 1024 ? 1024 : delay_ns;
+        while(tmp--); // nop
     }
     vPortExitCritical();
 }
