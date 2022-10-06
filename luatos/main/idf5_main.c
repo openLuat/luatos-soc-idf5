723924a9c78a3b3dd92db523be69531b0af19c17
Wendal Chen <wendal1985@gmail.com>
2022-10-06 15:44:01 +0800

    update: 若nvs初始化失败,自动格式化之

--- luatos/main/idf5_main.c
+++ luatos/main/idf5_main.c
@@ -12,6 +12,7 @@
 #include "esp_event.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
+#include "esp_log.h"
 
 
 extern int luat_main (void);
@@ -21,7 +22,12 @@ extern void luat_heap_init(void);
 void app_main(void)
 {
     bootloader_random_enable();
-    nvs_flash_init();
+    esp_err_t r = nvs_flash_init();
+    if (r == ESP_ERR_NVS_NO_FREE_PAGES || r == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        //ESP_LOGI("no free pages or nvs version mismatch, erase...");
+        nvs_flash_erase();
+        r = nvs_flash_init();
+    }
     luat_heap_init();
     esp_event_loop_create_default();
     luat_main();
