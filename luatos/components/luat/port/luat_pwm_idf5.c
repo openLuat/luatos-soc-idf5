3e49005e421cd0a3be860b02bee2699ddc3428f2
Dozingfiretruck <1473454180@qq.com>
2022-10-08 17:08:45 +0800

    add:gpio即为pwm号

--- luatos/components/luat/port/luat_pwm_idf5.c
+++ luatos/components/luat/port/luat_pwm_idf5.c
@@ -7,23 +7,23 @@
 #include "luat_log.h"
 #define LUAT_LOG_TAG "pwm"
 
-static uint8_t luat_pwm_idf[LEDC_TIMER_MAX] = {0};
+static int luat_pwm_idf[LEDC_TIMER_MAX] = {0};
 
 int luat_pwm_setup(luat_pwm_conf_t *conf){
-    int channel;
-    if (conf->channel > 9){
-        channel = conf->channel%10;
-    }else{
-        channel = conf->channel;
+    int timer = -1;
+    for (size_t i = 0; i < LEDC_TIMER_MAX; i++){
+        if (luat_pwm_idf[i]==0||luat_pwm_idf[i]==conf->channel+1){
+            timer = i;
+        }
     }
-    if (channel<0 || channel>=LEDC_TIMER_MAX) {
+    if (timer < 0) {
         return -1;
     }
-    if (luat_pwm_idf[channel] == 0){
+    if (luat_pwm_idf[timer] == 0){
         ledc_timer_config_t ledc_timer = {
             .speed_mode = LEDC_LOW_SPEED_MODE,
             .duty_resolution = LEDC_TIMER_13_BIT,
-            .timer_num = channel,
+            .timer_num = timer,
             .freq_hz = conf->period,
             .clk_cfg = LEDC_AUTO_CLK,
         };
@@ -31,174 +31,34 @@ int luat_pwm_setup(luat_pwm_conf_t *conf){
 
         ledc_channel_config_t ledc_channel = {
             .speed_mode = LEDC_LOW_SPEED_MODE,
-            .channel = channel,
-            .timer_sel = channel,
+            .channel = timer,
+            .timer_sel = timer,
             .intr_type = LEDC_INTR_DISABLE,
+            .gpio_num = conf->channel,
             .duty = 0,
             .hpoint = 0,
         };
-        switch (conf->channel){
-            case 00:
-                ledc_channel.gpio_num = 0;
-                break;
-            case 01:
-                ledc_channel.gpio_num = 1;
-                break;
-            case 02:
-                ledc_channel.gpio_num = 2;
-                break;
-            case 03:
-                ledc_channel.gpio_num = 3;
-                break;
-            case 10:
-                ledc_channel.gpio_num = 4;
-                break;
-            case 11:
-                ledc_channel.gpio_num = 5;
-                break;
-            case 12:
-                ledc_channel.gpio_num = 6;
-                break;
-            case 13:
-                ledc_channel.gpio_num = 7;
-                break;
-            case 20:
-                ledc_channel.gpio_num = 8;
-                break;
-            case 21:
-                ledc_channel.gpio_num = 9;
-                break;
-            case 22:
-                ledc_channel.gpio_num = 10;
-                break;
-            case 23:
-                ledc_channel.gpio_num = 11;
-                break;
-            case 30:
-                ledc_channel.gpio_num = 12;
-                break;
-            case 31:
-                ledc_channel.gpio_num = 13;
-                break;
-            case 32:
-                ledc_channel.gpio_num = 14;
-                break;
-            case 33:
-                ledc_channel.gpio_num = 15;
-                break;
-            case 40:
-                ledc_channel.gpio_num = 16;
-                break;
-            case 41:
-                ledc_channel.gpio_num = 17;
-                break;
-            case 42:
-                ledc_channel.gpio_num = 18;
-                break;
-            case 43:
-                ledc_channel.gpio_num = 19;
-                break;
-            case 50:
-                ledc_channel.gpio_num = 20;
-                break;
-            case 51:
-                ledc_channel.gpio_num = 21;
-                break;
-            default:
-                LLOGW("unkown pwm channel %d", channel);
-                return -1;
-        }
         ledc_channel_config(&ledc_channel);
-        luat_pwm_idf[channel] = 1;
+        luat_pwm_idf[timer] = conf->channel+1;//避免使用0
     }
-    ledc_set_freq(LEDC_LOW_SPEED_MODE, channel, conf->period);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, 8192 * conf->pulse / conf->precision );
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+    ledc_set_freq(LEDC_LOW_SPEED_MODE, timer, conf->period);
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, timer, 8192 * conf->pulse / conf->precision );
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, timer);
     return 0;
 }
 
 int luat_pwm_close(int channel){
-    int timer;
-    if (channel > 9){
-        timer = channel%10;
-    }else{
-        timer = channel;
+    int timer = -1;
+    for (size_t i = 0; i < LEDC_TIMER_MAX; i++){
+        if (luat_pwm_idf[i]==channel+1){
+            timer = i;
+        }
     }
-    if (timer<0 || timer>=LEDC_TIMER_MAX) {
+    if (channel < 0 || channel>=LEDC_TIMER_MAX || timer<0) {
         return -1;
     }
     ledc_stop(LEDC_LOW_SPEED_MODE, timer, 0);
-    switch (channel){
-        case 00:
-            gpio_reset_pin(0);
-            break;
-        case 01:
-            gpio_reset_pin(0);
-            break;
-        case 02:
-            gpio_reset_pin(0);
-            break;
-        case 03:
-            gpio_reset_pin(0);
-            break;
-        case 10:
-            gpio_reset_pin(0);
-            break;
-        case 11:
-            gpio_reset_pin(0);
-            break;
-        case 12:
-            gpio_reset_pin(0);
-            break;
-        case 13:
-            gpio_reset_pin(0);
-            break;
-        case 20:
-            gpio_reset_pin(0);
-            break;
-        case 21:
-            gpio_reset_pin(0);
-            break;
-        case 22:
-            gpio_reset_pin(0);
-            break;
-        case 23:
-            gpio_reset_pin(0);
-            break;
-        case 30:
-            gpio_reset_pin(0);
-            break;
-        case 31:
-            gpio_reset_pin(0);
-            break;
-        case 32:
-            gpio_reset_pin(0);
-            break;
-        case 33:
-            gpio_reset_pin(0);
-            break;
-        case 40:
-            gpio_reset_pin(0);
-            break;
-        case 41:
-            gpio_reset_pin(0);
-            break;
-        case 42:
-            gpio_reset_pin(0);
-            break;
-        case 43:
-            gpio_reset_pin(0);
-            break;
-        case 50:
-            gpio_reset_pin(0);
-            break;
-        case 51:
-            gpio_reset_pin(0);
-            break;
-        default:
-            LLOGW("unkown pwm channel %d", channel);
-            return -1;
-    }
+    gpio_reset_pin(0);
     luat_pwm_idf[timer] = 0;
     return 0;
 }
