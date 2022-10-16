cd5290356040e002ff25240a4007d11a9ac3567e
Wendal Chen <wendal1985@gmail.com>
2022-10-17 00:20:41 +0800

    fix: esp32的uart没有XTAL时钟源? 需要区分一下了,esp32使用APB

--- luatos/components/luat/port/luat_uart_idf5.c
+++ luatos/components/luat/port/luat_uart_idf5.c
@@ -112,7 +112,11 @@ int luat_uart_setup(luat_uart_t *uart){
     }
     uart_config.stop_bits = uart->stop_bits;
     uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+#ifndef CONFIG_IDF_TARGET_ESP32
     uart_config.source_clk = UART_SCLK_XTAL;
+#else
+    uart_config.source_clk = UART_SCLK_APB;
+#endif
     switch (id){
     case 0:
 #ifdef LUAT_USE_SHELL
