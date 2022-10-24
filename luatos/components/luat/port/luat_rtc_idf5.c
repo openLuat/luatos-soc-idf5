dae0a7ca5308ab462f82306e56400bb90620a395
Dozingfiretruck <1473454180@qq.com>
2022-10-24 17:28:56 +0800

    add:添加rtc定时唤醒

--- luatos/components/luat/port/luat_rtc_idf5.c
+++ luatos/components/luat/port/luat_rtc_idf5.c
@@ -2,6 +2,7 @@
 #include "luat_base.h"
 #include "luat_rtc.h"
 #include "sys/time.h"
+#include "esp_sleep.h"
 
 #include "luat_log.h"
 #define LUAT_LOG_TAG "rtc"
@@ -28,7 +29,12 @@ int luat_rtc_get(struct tm *tblock){
 }
 
 int luat_rtc_timer_start(int id, struct tm *tblock){
-    return -1; // 暂不支持
+    time_t now = {0};
+    time(&now);
+    time_t time_rtc = mktime(tblock);
+    uint64_t time_us = (time_rtc-now)*1000*1000;
+    esp_sleep_enable_timer_wakeup(time_us);
+    return 0;
 }
 
 int luat_rtc_timer_stop(int id){
