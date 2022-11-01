b94f20de379f0789037f0f504ee7e3237fcb6281
Dozingfiretruck <1473454180@qq.com>
2022-11-01 15:25:45 +0800

    fix:睡眠模式反了

--- luatos/components/luat/port/luat_pm_idf5.c
+++ luatos/components/luat/port/luat_pm_idf5.c
@@ -11,17 +11,19 @@
 
 int luat_pm_request(int mode) {
     if (mode == LUAT_PM_SLEEP_MODE_LIGHT) {
+        esp_light_sleep_start();
+        return 0;
+    }
+    else if (mode == LUAT_PM_SLEEP_MODE_DEEP || mode == LUAT_PM_SLEEP_MODE_STANDBY) {
+#if SOC_GPIO_SUPPORT_DEEPSLEEP_WAKEUP
         const gpio_config_t config = {
             .pin_bit_mask = BIT(DEFAULT_WAKEUP_PIN),
             .mode = GPIO_MODE_INPUT,
         };
         gpio_config(&config);
-        esp_sleep_enable_gpio_wakeup(BIT(DEFAULT_WAKEUP_PIN), ESP_GPIO_WAKEUP_GPIO_LOW);
+        esp_deep_sleep_enable_gpio_wakeup(BIT(DEFAULT_WAKEUP_PIN), ESP_GPIO_WAKEUP_GPIO_LOW);
+#endif
         esp_deep_sleep_start();
-        return 0;
-    }
-    else if (mode == LUAT_PM_SLEEP_MODE_DEEP || mode == LUAT_PM_SLEEP_MODE_STANDBY) {
-        esp_light_sleep_start();
         return 0;
     }
     return -1;
