5ee848e54dbb30d3c565f57a698460ab79e2b365
Wendal Chen <wendal1985@gmail.com>
2022-11-13 00:54:55 +0800

    fix: flash适配的地址判断错了

--- luatos/components/luat/port/luat_flash_idf5.c
+++ luatos/components/luat/port/luat_flash_idf5.c
@@ -11,14 +11,19 @@
 #include "esp_system.h"
 #include "esp_partition.h"
 
+#define LUAT_LOG_TAG "flash"
+#include "luat_log.h"
+
 
 extern const esp_partition_t * luadb_partition;
 
 int luat_flash_read(char* buff, size_t addr, size_t len) {
     if (luadb_partition == NULL) {
+        LLOGE("luadb_partition == NULL");
         return 0;
     }
-    if (luadb_partition->address < addr || addr >= luadb_partition->address + luadb_partition->size) {
+    if (luadb_partition->address > addr || addr >= luadb_partition->address + luadb_partition->size) {
+        LLOGW("read out of range");
         return 0;
     }
     if (addr + len > luadb_partition->address + luadb_partition->size) {
@@ -28,31 +33,42 @@ int luat_flash_read(char* buff, size_t addr, size_t len) {
     if (ret == ESP_OK) {
         return len;
     }
+    else {
+        LLOGW("read ret %d", ret);
+    }
     return 0;
 }
 
 int luat_flash_write(char* buff, size_t addr, size_t len) {
     if (luadb_partition == NULL) {
+        LLOGE("luadb_partition == NULL");
         return 0;
     }
-    if (luadb_partition->address < addr || addr >= luadb_partition->address + luadb_partition->size) {
+    //LLOGD("addr %08X size %04X luadb %08X size %08X", addr, len, luadb_partition->address, luadb_partition->size);
+    if (luadb_partition->address > addr || addr >= (luadb_partition->address + luadb_partition->size)) {
+        LLOGW("write out of range");
         return 0;
     }
-    if (addr + len > luadb_partition->address + luadb_partition->size) {
+    if ((addr + len) > (luadb_partition->address + luadb_partition->size)) {
         len = luadb_partition->address + luadb_partition->size - addr;
     }
     int ret = esp_partition_write(luadb_partition, addr - luadb_partition->address, buff, len);
     if (ret == ESP_OK) {
         return len;
     }
+    else {
+        LLOGW("write ret %d", ret);
+    }
     return 0;
 }
 
 int luat_flash_erase(size_t addr, size_t len) {
     if (luadb_partition == NULL) {
+        LLOGE("luadb_partition == NULL");
         return 0;
     }
-    if (luadb_partition->address < addr || addr >= luadb_partition->address + luadb_partition->size) {
+    if (luadb_partition->address > addr || (addr >= luadb_partition->address + luadb_partition->size)) {
+        LLOGW("erase out of range");
         return 0;
     }
     if (addr + len > luadb_partition->address + luadb_partition->size) {
@@ -61,6 +77,9 @@ int luat_flash_erase(size_t addr, size_t len) {
     int ret = esp_partition_erase_range(luadb_partition, addr - luadb_partition->address, len);
     if (ret == ESP_OK) {
         return len;
+    }
+    else {
+        LLOGW("erase ret %d", ret);
     }
     return 0;
 }
