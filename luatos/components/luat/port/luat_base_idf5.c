33da69de4c2fa66ad120fbf0384f76bda6541e3f
Wendal Chen <wendal1985@gmail.com>
2023-01-02 01:40:19 +0800

    add: 关联编译sfd库

--- luatos/components/luat/port/luat_base_idf5.c
+++ luatos/components/luat/port/luat_base_idf5.c
@@ -126,6 +126,9 @@ static const luaL_Reg loadedlibs[] = {
 #ifdef LUAT_USE_SFUD
   {"sfud", luaopen_sfud},              // sfud
 #endif
+#ifdef LUAT_USE_SFD
+  {"sfd", luaopen_sfd},              // sfud
+#endif
 #ifdef LUAT_USE_DISP
   {"disp",  luaopen_disp},              // OLED显示模块,支持SSD1306
 #endif
