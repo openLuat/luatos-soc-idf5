a004322967b892469d63d249e4cdcc7072e4a5c5
Dozingfiretruck <1473454180@qq.com>
2023-03-02 11:48:59 +0800

    add:添加max30102

--- luatos/components/luat/port/luat_base_idf5.c
+++ luatos/components/luat/port/luat_base_idf5.c
@@ -231,6 +231,9 @@ static const luaL_Reg loadedlibs[] = {
 #ifdef LUAT_USE_FATFS
   {"fatfs", luaopen_fatfs},
 #endif
+#ifdef LUAT_USE_MAX30102
+  {"max30102", luaopen_max30102},
+#endif
   {NULL, NULL}
 };
 
